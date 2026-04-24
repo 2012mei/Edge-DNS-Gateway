d8bf99e0e880c47fbd436feda185d8b503daf84b
Mei 2012 <mhayashi2012@outlook.jp>
2026-04-24 21:23:02 +0700

    Update update_lists.sh

--- update_lists.sh
+++ update_lists.sh
@@ -31,7 +31,7 @@ extract_domains() {
 
 echo "Downloading and processing blocklists..."
 curl -fsSL --max-time 60 \
-https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts \
+https://badmojr.github.io/1Hosts/Lite/domains.txt \
 | extract_domains > "$BLOCK_TMP"
 
 echo "Downloading and processing allowlists..."
